2c5513673c0666da810fbe596c79a65608dbf5c2
glib <guilduboc@gmail.com>
2019-03-05 15:45:49 +0100

    Add tags for expressions and define another lambda calculus from Victor's notes
    Also generalized the printing functions to both calculus

--- main.ml
+++ main.ml
@@ -1,30 +1,51 @@
 open Types
 
-let tt = Cst (`B true)
-let ff = Cst (`B false) 
-let one = Cst (`I 1)
-let zero = Cst (`I 0)
+let tt = `Cst (`B true)
+let ff = `Cst (`B false) 
+let one = `Cst (`I 1)
+let zero = `Cst (`I 0)
 let al = "α"
 let beta = "β"
-let x = Var "x"
+let x = `Var "x"
 let dyn_fun = `Arr (`Dyn, `Dyn)
 
 let _ =
     let examples = 
         [
-            Var "x";
-            Cst (`I 2);
-            Cst (`B true);
-            Lam (`Int, `Int, "x", Var "x");
-            Lam (`Dyn, `Bool, "x", tt);
-            App (Lam (`Int, `Int, "x", Var "x"), zero);
-            Prd (tt, ff);
-            Pi1 (Prd (tt, ff));
-            Pi2 (Prd (ff, tt));
-            Let ("x", tt, Var "x");
-            TLam ([al; beta], Lam (`TVar al, `TVar beta, "x", Var "x"));
-            Cast (Lam (`Dyn, `Dyn, "x", x), dyn_fun, Simple 0, `Arr (`Int, `Int));
-            Cast (Cast (Lam (`Dyn, `Dyn, "x", x), dyn_fun, Simple 0, `Arr (`Int, `Int)), `Arr (`Int, `Int), Simple 1, dyn_fun)
+            `Var "x";
+            `Cst (`I 2);
+            `Cst (`B true);
+            `Lam (`Int, `Int, "x", `Var "x");
+            `Lam (`Dyn, `Bool, "x", tt);
+            `App (`Lam (`Int, `Int, "x", `Var "x"), zero);
+            `Prd (tt, ff);
+            `Pi1 (`Prd (tt, ff));
+            `Pi2 (`Prd (ff, tt));
+            `Let ("x", tt, `Var "x");
+            `TLam ([al; beta], `Lam (`TVar al, `TVar beta, "x", `Var "x"));
+            `Cast (`Lam (`Dyn, `Dyn, "x", x), dyn_fun, Simple 0, `Arr (`Int, `Int));
+            `Cast (`Cast (`Lam (`Dyn, `Dyn, "x", x), dyn_fun, Simple 0, `Arr (`Int, `Int)), `Arr (`Int, `Int), Simple 1, dyn_fun)
         ] in
     print_string "Cast expressions:\n";
+    List.iter (fun e -> print_e e; print_endline "") examples
+
+let _ = 
+    let examples = 
+        [
+            (`Var "x" : e');
+            `Lam (`Empty, `Empty, "x", x);
+            (* `Cst (`I 2);
+            `Cst (`B true);
+            `Lam (`Int, `Int, "x", `Var "x");
+            `Lam (`Dyn, `Bool, "x", tt);
+            `App (`Lam (`Int, `Int, "x", `Var "x"), zero);
+            `Prd (tt, ff);
+            `Pi1 (`Prd (tt, ff));
+            `Pi2 (`Prd (ff, tt));
+            `Let ("x", tt, `Var "x");
+            `TLam ([al; beta], `Lam (`TVar al, `TVar beta, "x", `Var "x")); *)
+            `TwoCast (`Lam (`Dyn, `Dyn, "x", x), dyn_fun, `Arr (`Int, `Int));
+            `TwoCast (`TwoCast (`Lam (`Dyn, `Dyn, "x", x), dyn_fun, `Arr (`Int, `Int)), `Arr (`Int, `Int), dyn_fun)
+        ] in
+    print_string "\nOther cast expressions:\n";
     List.iter (fun e -> print_e e; print_endline "") examples
--- types.ml
+++ types.ml
@@ -1,5 +1,7 @@
 type alpha = string
 
+(** POPL19 types and cast expressions *)
+
 type t = [
     | `TVar of alpha
     | `Int 
@@ -17,8 +19,6 @@ type tau = [
     | `Arr of tau * tau
     ]
 
-(* Cast expressions *)
-
 type x = string
 type b = [ `I of int | `B of bool ]
 type alpha_vector = alpha list
@@ -29,19 +29,76 @@ type p =    (* blame label *)
     | Pos of int * int
     | Neg of int * int
 
-type e = 
-    | Var of x
-    | Cst of b
-    | Lam of tau * tau * x * e
-    | App of e * e
-    | Prd of e * e
-    | Pi1 of e
-    | Pi2 of e
-    | Let of x * e * e
-    | TLam of alpha_vector * e
-    | TApp of e * t_vector
-    | Cast of e * tau * p * tau
+type e = [
+    | `Var of x
+    | `Cst of b
+    | `Lam of tau * tau * x * e
+    | `App of e * e
+    | `Prd of e * e
+    | `Pi1 of e
+    | `Pi2 of e
+    | `Let of x * e * e
+    | `TLam of alpha_vector * e
+    | `TApp of e * t_vector
+    | `Cast of e * tau * p * tau
+    ]
 
+
+(* Victor's types and expressions from "Space-efficient [...]" notes *)
+
+type t' = [
+    | `TVar of alpha
+    | `Int 
+    | `Bool
+    | `Arr of t' * t'
+    | `Or of t' * t'
+    | `And of t' * t'
+    | `Neg of t'
+    | `Empty
+    ]
+
+type tau' = [ 
+    | `Dyn
+    | `TVar of alpha
+    | `Int 
+    | `Bool
+    | `Arr of tau' * tau'
+    | `Or of tau' * tau'
+    | `And of tau' * tau'
+    | `Neg of tau'
+    | `Empty
+    ]
+
+type e' = [
+    | `Var of x
+    | `Cst of b
+    | `Lam of tau' * tau' * x * e'
+    | `App of e' * e'
+    | `Prd of e' * e'
+    | `Pi1 of e'
+    | `Pi2 of e'
+    | `Let of x * e' * e'
+    | `TLam of alpha_vector * e'
+    | `TApp of e' * t_vector
+    | `TwoCast of e' * tau' * tau'
+    ]
+
+(* Boolean functions to determine the value among these terms *)
+type u = [ `Lam of tau' * tau' * x * e']
+type value = [ u | `Cst of b | `TwoCast of u * tau' * tau' ]
+
+let is_u : e' -> bool = function
+    | `Lam _ | `Cst _ -> true
+    | _ -> false
+
+let is_v : e' -> bool = fun e ->
+    if is_u e then true 
+    else match e with
+        | `TwoCast (`Lam _, _, _) -> true
+        | _ -> false
+
+
+(* Printing functions *)
 
 let pprint_p : p -> string = function 
     | Simple n -> string_of_int n
@@ -50,56 +107,65 @@ let pprint_p : p -> string = function
 let pprint_alpha_vector : alpha_vector -> string = 
     fun av -> "[" ^ (String.concat " ; " av) ^ "]"
 
-let rec pprint_t : t -> string = function
-    | `Bool -> "Bool"
-    | `TVar al -> al
-    | `Prod (t1, t2) -> Printf.sprintf "(%s, %s)" (pprint_t t1) (pprint_t t2)
-    | `Int -> "Int"
-    | `Arr (t1, t2) -> Printf.sprintf "%s 🡒 %s" (pprint_t t1) (pprint_t t2)
-
-let rec pprint_tau : tau -> string = function
+let rec pprint_type  = function
     | `Dyn -> "?"
     | `Bool -> "Bool"
     | `TVar al -> al
-    | `Prod (t1, t2) -> Printf.sprintf "(%s, %s)" (pprint_tau t1) (pprint_tau t2)
+    | `Prod (t1, t2) -> Printf.sprintf "(%s, %s)" (pprint_type t1) (pprint_type t2)
     | `Int -> "Int"
-    | `Arr (t1, t2) -> Printf.sprintf "%s 🡒 %s" (pprint_tau t1) (pprint_tau t2)
+    | `Arr (t1, t2) -> Printf.sprintf "%s 🡒  %s" (pprint_type t1) (pprint_type t2)
+    | `Or (t1, t2) -> Printf.sprintf "%s ∨ %s" (pprint_type t1) (pprint_type t2)
+    | `And (t1, t2) -> Printf.sprintf "%s ∧ %s" (pprint_type t1) (pprint_type t2)
+    | `Neg t1 -> Printf.sprintf "¬%s" (pprint_type t1) 
+    | `Empty -> "𝟘"
 
 let pprint_t_vector : t_vector -> string = fun tv ->   
-    let stv = List.map pprint_t tv in
+    let stv = List.map pprint_type tv in
     String.concat " ; " stv
 
-let rec pprint_e : e -> string = function
-    | Var x -> x
-    | Cst b -> (match b with
+let rec pprint_e = function
+    | `Var x -> x
+    | `Cst b -> (match b with
                 | `I n -> string_of_int n
                 | `B b -> string_of_bool b)
-    | Lam (t1, t2, x, e) -> 
-        Printf.sprintf "λ %s . %s : %s 🡒 %s" x (pprint_e e) (pprint_tau t1) (pprint_tau t2)
-    | App (e1, e2) -> 
+    | `Lam (t1, t2, x, e) -> 
+        Printf.sprintf "λ %s . %s : %s 🡒  %s" x (pprint_e e) (pprint_type t1) (pprint_type t2)
+    | `App (e1, e2) -> 
         Printf.sprintf "(%s) %s" (pprint_e e1) (pprint_e e2)
-    | Prd (e1, e2) ->
+    | `Prd (e1, e2) ->
         Printf.sprintf "(%s, %s)" (pprint_e e1) (pprint_e e2)
-    | Pi1 e -> 
+    | `Pi1 e -> 
         Printf.sprintf "π_1 %s" (pprint_e e)
-    | Pi2 e ->
+    | `Pi2 e ->
         Printf.sprintf "π_1 %s" (pprint_e e)
-    | Let (x, e1, e2) ->
+    | `Let (x, e1, e2) ->
         Printf.sprintf "let %s = %s in %s" x (pprint_e e1) (pprint_e e2)
-    | TLam (av, e) ->
+    | `TLam (av, e) ->
         Printf.sprintf "Λ %s . %s" (pprint_alpha_vector av) (pprint_e e)
-    | TApp (e, tv) ->
+    | `TApp (e, tv) ->
         Printf.sprintf "(%s) [%s]" (pprint_e e) (pprint_t_vector tv)
-    | Cast (e, tau1, p, tau2) ->    
-        match e with
-        | Lam _ -> Printf.sprintf "(%s) 〈%s ==[%s]==> %s〉" (pprint_e e) (pprint_tau tau1) (pprint_p p) (pprint_tau tau2) 
-        | _ -> Printf.sprintf "%s 〈%s ==[%s]==> %s〉" (pprint_e e) (pprint_tau tau1)  (pprint_p p) (pprint_tau tau2)
+    | `Cast (e, tau1, p, tau2) ->    
+        let s_format : _ format = 
+            (match e with
+            | `Lam _ ->     "(%s) 〈%s ==[%s]==> %s〉"
+            | _ ->          "%s 〈%s ==[%s]==> %s〉")
+        in Printf.sprintf s_format (pprint_e e) (pprint_type tau1) (pprint_p p) (pprint_type tau2) 
+        
+    | `TwoCast (e, tau1, tau2) ->    
+        let s_format : _ format = 
+            (match e with
+            | `Lam _ -> "(%s) 〈%s, %s〉"
+            | `TwoCast _ -> "%s〈%s, %s〉"
+            | _ -> "%s 〈%s, %s〉") in
+        Printf.sprintf s_format (pprint_e e) (pprint_type tau1) (pprint_type tau2)
 
-let print_e : e -> unit = 
+
+let print_e = 
     fun e -> print_string (pprint_e e)
 
 
-
-
-
-
+let typeof = function
+    | `Cst (`I _) -> `Int
+    | `Cst (`B _) -> `Bool 
+    | `Lam (tau1, tau2, _, _) -> `Arr (tau1, tau2)
+    | _ -> failwith "Not a value"
