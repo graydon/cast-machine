e5217c1d34b8a9ae0d861f0205642bc4b2132ce0
gduboc <guillaume.duboc@ens-lyon.fr>
2019-04-12 15:33:47 +0200

    Add forgotten source file

---
+++ src/utils.ml
@@ -0,0 +1,39 @@
+
+type parameters_structure =
+  {debug : bool ref;
+   symbolic : string ref;
+   machine: string ref;
+   load_file : bool ref;
+   verbose : int ref;
+   step_mode : bool ref;
+   step_start : int ref;
+   monitor : bool ref}
+
+let params =
+  {debug = ref false;
+  symbolic = ref "";
+  machine = ref "machine";
+  load_file = ref false;
+  verbose = ref 1;
+  step_mode = ref false;
+  step_start = ref 0;
+  monitor = ref false}
+
+let rec find a x n =
+if a.(n) = x then n 
+else find a x (n+1)
+
+let cmp_tuple (a,b) (c,d) =
+  if b > d then 1 
+  else if b < d then -1
+  else 0
+
+let printf = Printf.printf
+let flush = Pervasives.flush stdout
+
+let max cmp l = 
+  let rec aux acc = function
+  | [] -> acc
+  | x :: t when cmp x acc > 0 -> aux x t
+  | x :: t -> aux acc t
+  in aux (List.hd l) l
