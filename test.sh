c0c1a25c79cd4f6a108a49f40143ce407cfc7233
gduboc <guillaume.duboc@ens-lyon.fr>
2019-04-28 16:38:28 +0200

    Add expected output to unit tests

--- test.sh
+++ test.sh
@@ -4,6 +4,20 @@ files=$(ls ./tests)
 
 for f in ${files}; do
     echo "testing $f"
-    ./cast.exe --load "./tests/$f"
-    echo ""
+
+    out="- :$(cat "tests/$f" | grep "(\*\*" | awk '{$1="";$NF=""; print $0}')"
+    echo "expected output:"
+    echo "{${out}}"
+
+    rout="$(./cast.exe --load "./tests/$f") "
+    echo "computed output:"
+    echo "{${rout}}"
+
+    if [ "${out}" = "${rout}" ]; then
+	echo "success"
+    else
+	echo "wrong output"
+	echo "terminating test session"
+	exit
+    fi
 done
